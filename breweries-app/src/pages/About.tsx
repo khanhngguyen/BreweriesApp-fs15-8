901cd73121b1307b1e8c76564e726b6108a43b1a
khanhngguyen <ntkimkhanh97@gmail.com>
2023-05-08 15:39:53 +0300

    added styles for Contact page

--- breweries-app/src/pages/About.tsx
+++ breweries-app/src/pages/About.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
-    <div>
+    <div className='about-page'>
         <h2>About Breweries</h2>
         <p>An app to fetch, display, and search for brewery companies from the given API endpoint, using React and TypeScript</p>
         <p><Link to='/'>Back</Link></p>
--- breweries-app/src/pages/Contact.tsx
+++ breweries-app/src/pages/Contact.tsx
@@ -3,29 +3,29 @@ import { Link, Form } from 'react-router-dom';
 
 const Contact = () => {
   return (
-    <div>
+    <div className='contact-page'>
+        <div className= 'title'>
+
         <p><Link to='/'>Back</Link></p>
         <h2>Contact form:</h2>
         <p>Leave your contact & message in the form below</p>
+        </div>
         <Form method='post' action='/contact'>
-            <label htmlFor='email'>
-                <span>Email:</span>
-                <input 
-                type='email' 
-                name='email' 
-                placeholder='Your email'
-                required 
-                />
-            </label>
-            <label htmlFor='message'>
-                <span>Message:</span>
-                <input
-                type='textarea'
-                name='message'
-                placeholder='Your message'
-                required
-                />
-            </label>
+            <label htmlFor='email'>Email:</label>
+            <input 
+            type='email' 
+            name='email' 
+            placeholder='Your email'
+            required 
+            />
+            <label htmlFor='message'>Message: </label>
+            <textarea
+            // type='textarea'
+            name='message'
+            rows={3}
+            placeholder='Your message'
+            required
+            />
             <button type='submit'>Submit</button>
         </Form>
     </div>
@@ -35,7 +35,7 @@ const Contact = () => {
 export default Contact
 
 export const contactSubmit = async ({ request }: { request: any }) => {
-    console.log(request);
+    // console.log(request);
     const data = await request.formData();
     const submission = {
         email: data.get('email'),
